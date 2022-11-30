e6bab4de2e466f9bc01ec1dedd4cf817475141df
eric-zheng <yuxuan.zheng@mail.utoronto.ca>
2022-11-30 10:40:01 -0500

    Improve the messages of both scripts

--- classical/solver_mip.py
+++ classical/solver_mip.py
@@ -6,7 +6,8 @@ import sys
 import argparse
 import os
 
-sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), "..")))
+project_path = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_path)
 
 import numpy as np
 import cvxpy as cp
@@ -22,7 +23,7 @@ batch_size = 1
 
 def load_pretrained_model():
     """Load the pre-trained model and fetch a single image from the test dataset."""
-    project_path = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+    logger.info(f"Loading the pre-trained neural network model...")
     logger.debug("Project path: {}".format(project_path))
 
     data_path = os.path.join(project_path, "data")
@@ -46,6 +47,7 @@ def load_pretrained_model():
 
 def get_model_params(model):
     """Get the parameters of the model."""
+    logger.info(f"Loading the model parameters...")
     weights_and_bias = []
     weights = []
     bias = []
@@ -59,8 +61,8 @@ def get_model_params(model):
         else:
             bias.append(weights_and_bias[i].reshape(1, -1))
 
-    logger.info("Num of weights: {}".format(len(weights)))
-    logger.info("Num of biases: {}".format(len(bias)))
+    logger.debug("Num of weights: {}".format(len(weights)))
+    logger.debug("Num of biases: {}".format(len(bias)))
 
     return weights, bias
 
@@ -69,6 +71,7 @@ def construct_math_program(model, img, weights, bias):
     """
     Construct the math program to solve.
     """
+    logger.info(f"Constructing the math program to solve...")
     perturb = cp.Variable((1, 784))
     constraints = [-0.1 <= perturb, perturb <= 0.1]
     x = img + perturb
@@ -78,9 +81,16 @@ def construct_math_program(model, img, weights, bias):
     a = []
     y_list = []
 
+    logger.info("")
+    logger.info(
+        f"Stage 1: solve lower/upper bounds of Linear layers and append constraints for ReLU layers..."
+    )
+
     for name, layer in model.named_modules():
         if type(layer) is nn.Linear:
-            logger.info("Considering: name: {}, layer: {}".format(name, layer))
+            # If we hit a Linear layer, we need to solve an LP to get the lower and upper bound of the activations of this layer.
+            # The lower and upper bound are used in the next ReLU layer to construct constraints for the final LP problem.
+            logger.info("Considering: {}, layer: {}".format(name, layer))
             x = x @ weights[layer_idx].T + bias[layer_idx]
             row, col = x.shape
             lb_temp = np.zeros_like(x, shape=x.shape)
@@ -103,6 +113,8 @@ def construct_math_program(model, img, weights, bias):
 
             layer_idx += 1
         elif type(layer) is nn.ReLU:
+            # If we hit a ReLU layer, we want to append multiple constraints (related to the lower and upper bound of the previous Linear layer)
+            # to the constraints set.
             logger.info("Considering: name: {}, layer: {}".format(name, layer))
             a_temp = cp.Variable(x.shape, boolean=True)
             row, col = x.shape
@@ -119,10 +131,18 @@ def construct_math_program(model, img, weights, bias):
             y_list.append(y)
             a.append(a_temp)
 
-    # Define the objective function
+    # Define the final LP problem to solve
+    logger.info("")
+    logger.info(f"Stage 2: constructing the final LP to solve...")
+
+    # For this particular image, the correct predication if class 7
+    # Here, we are trying to minimize the predication probability between class 7 and class 2,
+    # meaning that we are trying to make the model to make a very wrong prediction towards class 2.
+    # And we want to see if we can successfully fool the model to change the predication from class 7 to class 2.
     obj = cp.Minimize(x[0][7] - x[0][2])
     prob = cp.Problem(obj, constraints)
 
+    logger.info(f"...done")
     return prob, perturb
 
 
@@ -133,28 +153,53 @@ def main(args):
 
     prob, perturb = construct_math_program(model, img, weights, bias)
 
+    logger.info("")
+    logger.info(f"Solving the final LP...")
     optimal_value = None
     if args.CPLEX:
-        optimal_value = prob.solve(solver=cp.CPLEX, cplex_filename='model.lp')
+        optimal_value = prob.solve(
+            solver=cp.CPLEX,
+            cplex_filename=os.path.join(project_path, "model.lp"),
+        )
     else:
         optimal_value = prob.solve()
-        
+
     logger.info("Optimal value reported by the classical solver: {}".format(optimal_value))
+    if optimal_value > 0:
+        logger.info(
+            "The optimal value is positive, meaning that the model cannot be fooled to make a wrong prediction by manipulating the image."
+        )
+    else:
+        logger.info(
+            "The optimal value is negative, meaning that the model may be fooled to make a wrong prediction (from class 7 to class 2) by manipulating the image."
+        )
 
     # Compare the prediction result of the original and perturbed image
     img_new = img + perturb.value
     img_new = torch.from_numpy(img_new).float().cpu()
     original_pred = model(torch.from_numpy(img).float().cpu())
     perturbed_pred = model(img_new)
+    original_class = torch.argmax(original_pred)
+    perturbed_class = torch.argmax(perturbed_pred)
 
-    logger.info(f"Prediction given the original image:\n{original_pred}")
-    logger.info(f"Prediction given the perturbed image:\n{perturbed_pred}")
+    logger.info(
+        f"Probability distribution over 10 classes given the original image:\n{original_pred.detach().numpy()}"
+    )
+    logger.info(
+        f"Probability distribution over 10 classes given the perturbed image:\n{perturbed_pred.detach().numpy()}"
+    )
+    logger.info(f"Prediction given the original image: class {original_class}")
+    logger.info(f"Prediction given the perturbed image: class {perturbed_class}")
+    if original_class == perturbed_class:
+        logger.info(f"No adversarial input exists for this model.")
+    else:
+        logger.info(f"There exists adversarial inputs for this model.")
 
 
 def get_input_args():
     """Create and parse arguments."""
     parser = argparse.ArgumentParser(
-        description="Classical solver to the adversial attack problem.")
+        description="Classical solver to the adversarial attack problem.")
 
     parser.add_argument(
         "-v",
@@ -162,12 +207,11 @@ def get_input_args():
         action="store_true",
         help="show debug messages",
     )
-
     parser.add_argument(
         "-C",
         "--CPLEX",
         action="store_true",
-        help="Use CPLEX solver and write the model to a .lp file.",
+        help="use the CPLEX solver and write the model to a .lp file",
     )
 
     args = parser.parse_args()
@@ -176,7 +220,7 @@ def get_input_args():
 
 if __name__ == "__main__":
     args = get_input_args()
-    logger = setup_logger("main", verbose=args.verbose)
+    logger = setup_logger("classical", verbose=args.verbose)
     logger.debug("Received input argument: {}".format(args))
 
     try:
--- quantum/solver_dwave.py
+++ quantum/solver_dwave.py
@@ -6,7 +6,8 @@ import sys
 import argparse
 import os
 
-sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), "..")))
+project_path = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_path)
 
 import numpy as np
 import cvxpy as cp
@@ -23,9 +24,10 @@ import pickle
 
 batch_size = 1
 
+
 def load_pretrained_model():
     """Load the pre-trained model and fetch a single image from the test dataset."""
-    project_path = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+    logger.info(f"Loading the pre-trained neural network model...")
     logger.debug("Project path: {}".format(project_path))
 
     data_path = os.path.join(project_path, "data")
@@ -49,6 +51,7 @@ def load_pretrained_model():
 
 def get_model_params(model):
     """Get the parameters of the model."""
+    logger.info(f"Loading the model parameters...")
     weights_and_bias = []
     weights = []
     bias = []
@@ -62,14 +65,14 @@ def get_model_params(model):
         else:
             bias.append(weights_and_bias[i].reshape(1, -1))
 
-    logger.info("Num of weights: {}".format(len(weights)))
-    logger.info("Num of biases: {}".format(len(bias)))
+    logger.debug("Num of weights: {}".format(len(weights)))
+    logger.debug("Num of biases: {}".format(len(bias)))
 
     return weights, bias
 
+
 def parse_sample(sample):
     perturb = np.zeros(784)
-
     variables = sample.sample
 
     for i in range(784):
@@ -77,43 +80,66 @@ def parse_sample(sample):
 
     return perturb
 
+
 def main(args):
     """Main logic"""
     model, img = load_pretrained_model()
 
+    TIME_LIMIT = 60
+
+    logger.info("")
+    logger.info(f"Solving the final LP with D-Wave...")
+
     with open('model.lp', 'rb') as f:
-            cqm = dimod.lp.load(f)
+        cqm = dimod.lp.load(f)
 
-            if args.sampleset:
-                with open('sampleset', 'wb') as sampleset_file:
-                    sampler = LeapHybridCQMSampler() 
-                    sampleset = sampler.sample_cqm(cqm, time_limit=60, label="18819 - Barebones code for solving CQM")
-                    sampleset_file.write(pickle.dumps(sampleset.to_serializable(use_bytes = True)))
+        if args.sampleset:
+            with open('sampleset', 'wb') as sampleset_file:
+                sampler = LeapHybridCQMSampler()
+                sampleset = sampler.sample_cqm(
+                    cqm,
+                    time_limit=TIME_LIMIT,
+                    label="18819 - Barebones code for solving CQM",
+                )
+                sampleset_file.write(pickle.dumps(sampleset.to_serializable(use_bytes=True)))
 
-            unpickle_file = open('sampleset', 'rb')
-            sampleset = dimod.SampleSet.from_serializable(pickle.load(unpickle_file))
-                                
-            logger.info("{} feasible solutions of {}.".format(
-                sampleset.record.is_feasible.sum(), len(sampleset)))   
-            
-            logger.info(f"Best sample: {sampleset.filter(lambda row: row.is_feasible).first}")
+        logger.info("...done")
+        unpickle_file = open('sampleset', 'rb')
+        sampleset = dimod.SampleSet.from_serializable(pickle.load(unpickle_file))
 
-            perturb = parse_sample(sampleset.filter(lambda row: row.is_feasible).first)
+        logger.info("{} feasible solutions of {}.".format(sampleset.record.is_feasible.sum(),
+                                                          len(sampleset)))
+        logger.info(f"Best sample: {sampleset.filter(lambda row: row.is_feasible).first}")
 
-            # Compare the prediction result of the original and perturbed image
-            img_new = img + perturb
-            img_new = torch.from_numpy(img_new).float().cpu()
-            original_pred = model(torch.from_numpy(img).float().cpu())
-            perturbed_pred = model(img_new)
+        perturb = parse_sample(sampleset.filter(lambda row: row.is_feasible).first)
 
-            logger.info(f"Prediction given the original image:\n{original_pred}")
-            logger.info(f"Prediction given the perturbed image:\n{perturbed_pred}")
+        # Compare the prediction result of the original and perturbed image
+        img_new = img + perturb
+        img_new = torch.from_numpy(img_new).float().cpu()
+        original_pred = model(torch.from_numpy(img).float().cpu())
+        perturbed_pred = model(img_new)
+        original_class = torch.argmax(original_pred)
+        perturbed_class = torch.argmax(perturbed_pred)
+
+        logger.info("")
+        logger.info(
+            f"Probability distribution over 10 classes given the original image:\n{original_pred.detach().numpy()}"
+        )
+        logger.info(
+            f"Probability distribution over 10 classes given the perturbed image:\n{perturbed_pred.detach().numpy()}"
+        )
+        logger.info(f"Prediction given the original image: class {original_class}")
+        logger.info(f"Prediction given the perturbed image: class {perturbed_class}")
+        if original_class == perturbed_class:
+            logger.info(f"No adversarial input exists for this model.")
+        else:
+            logger.info(f"There exists adversarial inputs for this model.")
 
 
 def get_input_args():
     """Create and parse arguments."""
     parser = argparse.ArgumentParser(
-        description="Classical solver to the adversial attack problem.")
+        description="Quantum solver to the adversarial attack problem.")
 
     parser.add_argument(
         "-v",
@@ -121,7 +147,6 @@ def get_input_args():
         action="store_true",
         help="show debug messages",
     )
-
     parser.add_argument(
         "-s",
         "--sampleset",
@@ -135,7 +160,7 @@ def get_input_args():
 
 if __name__ == "__main__":
     args = get_input_args()
-    logger = setup_logger("main", verbose=args.verbose)
+    logger = setup_logger("quantum", verbose=args.verbose)
     logger.debug("Received input argument: {}".format(args))
 
     try:
